7523a91621b9a009fd34616b1c1e604e072395dd
Elias Ablan <eliasablan93@gmail.com>
2025-09-04 00:04:17 -0400

    add next-themes to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/theme-dropdown";
 import Image from "next/image";
 
 export default function Home() {
@@ -97,6 +98,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
+        <ModeToggle />
       </footer>
     </div>
   );
